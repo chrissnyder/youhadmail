df96532516977e8a4a9fe03e0150ca1c5c3087ac
Shaun Chapman <shaunchapman@gmail.com>
2013-02-14 16:34:27 -0600

    Working on the front page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def active_at(*locations)
+    locations.each do |location|
+      current_controller, current_action = location.split('#')
+
+      if current_action
+        if [params[:controller], params[:action]] == [current_controller, current_action]
+          return 'active'
+        end
+      elsif controller_name == current_controller
+        return 'active'
+      end
+    end
+
+    return nil
+  end
 end
